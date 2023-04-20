8ae291755e72cc37c53c4718a20dfaa056ad1f57
season <season.saw@gmail.com>
2023-04-20 16:22:32 +0800

    BAD commit. Unable to compile

--- user.cmd.api/src/main/java/com/springbank/user/cmd/api/UserCommandApplication.java
+++ user.cmd.api/src/main/java/com/springbank/user/cmd/api/UserCommandApplication.java
@@ -1,9 +1,12 @@
 package com.springbank.user.cmd.api;
 
+import com.springbank.user.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserCommandApplication {
 
     public static void main(String[] args) {
