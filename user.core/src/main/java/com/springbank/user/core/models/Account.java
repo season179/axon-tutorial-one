40bac3638cf78b0e76068dcea416c203b22f1c16
season <season.saw@gmail.com>
2023-04-22 13:15:06 +0800

    Added account and user model. Added role enum

---
+++ user.core/src/main/java/com/springbank/user/core/models/Account.java
@@ -0,0 +1,21 @@
+package com.springbank.user.core.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Account {
+
+    private String username;
+
+    private String password;
+
+    private List<Role> roles;
+}
---
+++ user.core/src/main/java/com/springbank/user/core/models/User.java
@@ -0,0 +1,27 @@
+package com.springbank.user.core.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String emailAddress;
+
+    private Account account;
+}
