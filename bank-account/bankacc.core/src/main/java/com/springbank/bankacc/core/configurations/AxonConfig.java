5a367ccde0ea9797f80b8015f4a49c32708375c1
season <season.saw@gmail.com>
2023-04-24 20:34:31 +0800

    Set up project for bank account.

---
+++ bank-account/bankacc.core/src/main/java/com/springbank/bankacc/core/configurations/AxonConfig.java
@@ -0,0 +1,92 @@
+package com.springbank.bankacc.core.configurations;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.axonframework.extensions.mongo.DefaultMongoTemplate;
+import org.axonframework.extensions.mongo.MongoTemplate;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
+import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
+import org.axonframework.serialization.Serializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+public class AxonConfig {
+
+    @Value("${spring.data.mongodb.host:127.0.0.1}")
+    private String mongoHost;
+
+    @Value("${spring.data.mongodb.port:27017}")
+    private int mongoPort;
+
+    @Value("${spring.data.mongodb.database:bank}")
+    private String mongoDatabase;
+
+    @Bean
+    public MongoClient mongo() {
+        var mongoClientSettings = MongoClientSettings.builder()
+                .applyToClusterSettings(builder -> builder.hosts(Collections.singletonList(
+                        new ServerAddress(this.mongoHost, this.mongoPort))))
+//                .credential(MongoCredential.createCredential("username", "database", "password".toCharArray()))
+                .build();
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate axonMongoTemplate() {
+        return DefaultMongoTemplate.builder()
+                .mongoDatabase(mongo(), mongoDatabase)
+                .build();
+    }
+
+    @Bean
+    public TokenStore tokenStore(Serializer serializer) {
+        return MongoTokenStore.builder()
+                .mongoTemplate(axonMongoTemplate())
+                .serializer(serializer)
+                .build();
+    }
+
+    @Bean
+    public EventStorageEngine storageEngine(MongoClient client) {
+        return MongoEventStorageEngine.builder()
+                .mongoTemplate(DefaultMongoTemplate.builder()
+                        .mongoDatabase(client)
+                        .build())
+                .build();
+    }
+
+    @Bean
+    public EmbeddedEventStore eventStore(EventStorageEngine storageEngine, org.axonframework.config.Configuration configuration) {
+        return EmbeddedEventStore.builder()
+                .storageEngine(storageEngine)
+                .messageMonitor(configuration.messageMonitor(EventStore.class, "eventStore"))
+                .build();
+    }
+
+    @Bean
+    public XStream xStream() {
+        XStream xStream = new XStream(new DomDriver());
+        XStream.setupDefaultSecurity(xStream);
+        xStream.allowTypesByWildcard(new String[]{
+                "com.springbank.bankacc.core.models.**",
+                "com.springbank.bankacc.query.api.queries.**",
+                "com.springbank.bankacc.query.api.dto.**",
+                "com.springbank.bankacc.cmd.api.commands.**",
+                "com.springbank.bankacc.cmd.api.dto.**",
+        });
+        return xStream;
+    }
+
+}
